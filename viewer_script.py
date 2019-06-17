0e79b3c6fd9e934fe09c4814e1b2d3240b886f72
lee-vivian <lee-vivian@users.noreply.github.com>
2019-06-16 23:08:35 -0400

    Fix bug in sum_view_dists_by_user

--- viewer_script.py
+++ viewer_script.py
@@ -365,6 +365,7 @@ def sum_view_dists_by_user(cluster_mapping, views, stats=False, square=False):
 				dist = [sum(x) for x in zip(view_distribution, dist)] # add
 			current_user = key_to_uid(key)
 			users_views = {}
+			users_views[key] = views[key]
 	# finally
 	view_distribution = views_to_normalized_cluster_distribution(users_views, cluster_mapping)
 	if current_user in EXPERTS:
